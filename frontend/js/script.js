d575631e6b99151f6840bde7588660d0260e017e
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-05 11:41:34 -0300

    update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -10,16 +10,16 @@ async function buscarEMostrarVideos() {
         throw new Error('Vídeo não tem categoria');
       }
       containerVideos.innerHTML += `
-          <li class="videos__item">
-              <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-              <div class="descricao-video">
-                  <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
-                  <h3 class="titulo-video">${video.titulo}</h3>
-                  <p class="titulo-canal">${video.descricao}</p>
-                  <p class="categoria" hidden>${video.categoria}</p>
-              </div>
-          </li>
-          `;
+        <li class="videos__item">
+          <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+          <div class="descricao-video">
+            <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+            <h3 class="titulo-video">${video.titulo}</h3>
+            <p class="titulo-canal">${video.descricao}</p>
+            <p class="categoria" hidden>${video.categoria}</p>
+          </div>
+        </li>
+      `;
     })
   } catch (error) {
     containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
